a2883553a1a5584bc01bddc683d50183228c99d6
chan3785 <97221124+chan3785@users.noreply.github.com>
2025-06-02 23:21:44 +0900

    Title: 2021년 날짜의 유무, Time: 40ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250602/2021년 날짜의 유무/with-or-without-2021.py
@@ -0,0 +1,26 @@
+m, d = map(int, input().split())
+
+# Please write your code here.
+def is_날짜있음(m, d):
+    if d > 31 or m > 12:
+        return "No"
+    if m > 7:
+        if m % 2 == 0:
+            if d <= 30:
+                return "Yes"
+        else:
+            if d <= 30:
+                return "Yes"
+    elif m < 7:
+        if m == 2:
+            if d <= 28:
+                return "Yes"
+        elif m % 2 == 0:
+            if d <= 30:
+                return "Yes"
+        else:
+            if d <= 31:
+                return "Yes"
+    return "No"
+
+print(is_날짜있음(m,d))
